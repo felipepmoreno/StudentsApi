4f7bd2eb333b759ad1821442e69882253368a9d8
Felipe Pereira Moreno <felipe.moreno@celcoin.com.br>
2021-12-03 11:20:26 -0300

    Add project files.

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AlunosApi.Models;
+
+namespace AlunosApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+        //Este contexto deve ser declarado no Startup informando o provedor que será utilizado e a string de conexão
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Aluno> Alunos { get; set; }
+    }
+}
