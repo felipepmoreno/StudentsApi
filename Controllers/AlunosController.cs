c731d949533df7be640437abe78f696ffbf668da
felipepmoreno <felipepmoreno@gmail.com>
2021-12-07 21:40:25 -0300

    implementação do post e put

--- Controllers/AlunosController.cs
+++ Controllers/AlunosController.cs
@@ -27,7 +27,7 @@ namespace AlunosApi.Controllers
             _alunoService = alunoService;
         }
 
-        [HttpGet]
+        [HttpGet("Alunos")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<IAsyncEnumerable<Aluno>>> GetAlunos()
@@ -39,8 +39,79 @@ namespace AlunosApi.Controllers
             }
             catch
             {
-                //return BadRequest("Request Inválido");
                 return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao obter alunos");
+            }
+        }
+        [HttpGet("AlunoPorNome")]
+        public async Task<ActionResult<IAsyncEnumerable<Aluno>>> GetAlunosByName([FromQuery] string nome)
+        {
+            try
+            {
+                var alunos = await _alunoService.GetAlunosByNome(nome);
+
+                if (alunos.Count() == 0)
+                    return NotFound($"Não existem alunos com o critério {nome}");
+
+                return Ok(alunos);
+            }
+            catch
+            {
+                return BadRequest("Request Inválido");
+            }
+
+        }
+
+        [HttpGet("{id:int}", Name="GetAlunoId")]
+        public async Task<ActionResult<Aluno>> GetAluno(int id)
+        {
+            try
+            {
+                var aluno = await _alunoService.GetAluno(id);
+
+                if (aluno == null)
+                    return NotFound($"Não existem alunos com id {id}");
+
+                return Ok(aluno);
+            }
+            catch
+            {
+                return BadRequest("Request Inválido");
+            }
+        }
+
+        //um método post deve retornar 201 (CreateAtRoute) para informar que um recurso foi criado
+        [HttpPost]
+        public async Task<ActionResult<Aluno>> Create(Aluno aluno)
+        {
+            try
+            {
+                await _alunoService.CreateAluno(aluno);
+                return CreatedAtRoute(nameof(GetAluno), new { id = aluno.Id }, aluno);
+            }
+            catch
+            {
+                return BadRequest("Request Inválido");
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Edit(int id, [FromBody]Aluno aluno)
+        {
+            try
+            {
+                if(aluno.Id == id)
+                {
+                    await _alunoService.UpdateAluno(aluno);
+                    return NoContent();
+                }
+                else
+                {
+                    return BadRequest("Dados de aluno Inválidos");
+                }
+            }
+            catch
+            {
+                return BadRequest("Request Inválido");
             }
         }
     }
